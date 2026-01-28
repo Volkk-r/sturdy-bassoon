33b8592cf46900416f58ebf5dc0d64774d4c0e33
Anna <piouiyutyrt@kjlhkgj.ry>
2026-01-28 18:33:35 +0300

    Задачи на условия

--- js/task.js
+++ js/task.js
@@ -4,6 +4,26 @@ let b = 9;
 let sum = a + b;
 let diff = a - b;
 
+console.log(sum, diff);
+console.log(diff);
 
-console.log(sum, diff)
-console.log(diff)
+// Урок 2: Операторы сравнения и логические операторы
+
+let title = "Прочитать конспект";
+
+if (title === "") {
+  console.log("Название задачи не указано");
+} else {
+  console.log("Название задачи:", title);
+}
+
+
+let tasks = 5;
+if (tasks === 0) {
+    console.log("Нет задач для выполнения");
+} else if (tasks > 0 && tasks <= 3) {
+    console.log("Небольшое количество задач");
+} else if (tasks > 3) {
+    console.log("Много задач");
+}
+
